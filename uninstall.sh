5ab01efa243006879f2ff1ceb4a068238c67ba6e
Khant-Nyar <khantsithuphyo2001@gmail.com>
2023-06-23 18:57:36 +0630

    change with textfile

--- uninstall.sh
+++ uninstall.sh
@@ -1,27 +1,14 @@
 #!/bin/bash
 
 # Read the list of software to uninstall from a text file
-mapfile -t software < software_list.txt
+mapfile -t software < uninstall.txt
 
 # Iterate over the list and uninstall each software
 for software_name in "${software[@]}"; do
   echo "Uninstalling $software_name"
   sudo apt-get remove --purge -y "$software_name"
+  sudo apt -y autoremove 
 done
 
-# Install Sublime Text, VS Code, Postman, Warp CLI, TablePlus, and LAMP stack (if desired)
-# Install Fonts from GitHub
-echo "Installing Fonts from GitHub"
-mkdir -p ~/.fonts
-cd ~/.fonts
-wget https://github.com/user/repo/raw/main/font-file.ttf
-fc-cache -f -v
-
-# Install Chromium
-echo "Installing Chromium"
-sudo apt-get install -y chromium-browser
-
-echo "Installation complete."
-
-
+echo "Uninstallation complete."
 
